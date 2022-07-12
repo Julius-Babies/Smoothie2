c3bc2d1b4f916168260483e9a2bfc8080c2f7911
Julius Babies <julius@familie-babies.de>
2022-07-12 18:46:57 +0200

    Doch wieder auf TS umgestiegen + eigener AJAX Client

---
+++ js/tools.js
@@ -0,0 +1,56 @@
+// export for others scripts to use
+export function getCookie(cname) {
+    let name = cname + "=";
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let ca = decodedCookie.split(';');
+    for (let i = 0; i < ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) === ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) === 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+export function setCookie(name, value, expire_days) {
+    const d = new Date();
+    d.setTime(d.getTime() + (expire_days * 24 * 60 * 60 * 1000));
+    let expires = "expires=" + d.toUTCString();
+    document.cookie = name + "=" + value + ";" + expires + ";path=/";
+}
+export function query(type, data = "") {
+    return ajax("./querymaster.php", "POST", `type=${type}&data=${data}`, false);
+}
+export function addLeadingZeros(num, totalLength) {
+    return String(num).padStart(totalLength, '0');
+}
+export function ajax(url, method, data, async) {
+    method = typeof method !== 'undefined' ? method : 'GET';
+    async = typeof async !== 'undefined' ? async : false;
+    let xhReq;
+    if (window.XMLHttpRequest) {
+        xhReq = new XMLHttpRequest();
+    }
+    else {
+        xhReq = new ActiveXObject("Microsoft.XMLHTTP");
+    }
+    if (method == 'POST') {
+        xhReq.open(method, url, async);
+        xhReq.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+        xhReq.setRequestHeader("X-Requested-With", "XMLHttpRequest");
+        xhReq.send(data);
+    }
+    else {
+        if (typeof data !== 'undefined' && data !== null) {
+            url = url + '?' + data;
+        }
+        xhReq.open(method, url, async);
+        xhReq.setRequestHeader("X-Requested-With", "XMLHttpRequest");
+        xhReq.send(null);
+    }
+    return xhReq.responseText;
+    //alert(serverResponse);
+}
+//# sourceMappingURL=tools.js.map
